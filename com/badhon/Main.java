f6cda2524a51dc992a05b64f9bcf19f621469ed6
aashiq4dev <ashiku161612@gmail.com>
2021-08-11 18:13:51 +0600

    Multi Dimantion Array

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        // Multi Dimantion Array
        
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        
       
     
     }
