57405fde5106355895691868abbb5307204c65bf
aashiq4dev <ashiku161612@gmail.com>
2021-08-11 17:30:55 +0600

    Ecape Sequence class Start

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -3,12 +3,7 @@ package com.badhon;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        String masseage = "Hello World" + "!!";
-       
-
-        System.out.println(masseage.trim());
-        //length, toUperCase, toLowerCase, trim, indexOf set Method
-        System.out.println(masseage);
+        //Ecape Sequence
     }
 
     
