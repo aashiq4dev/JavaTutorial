a6ed5f0a8b7ad55ee5e5c5ad6bbf3854256a21ce
aashiq4dev <ashiku161612@gmail.com>
2021-08-11 18:24:11 +0600

    Arithmatic Expressions start

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -5,10 +5,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        // Constants in java
-       
-        final float PI = 3.14F;
-       
+        // Arithmatic Expressions
+        
 
       
     
