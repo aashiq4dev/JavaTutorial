c887adc78705132998bebe7d6cadd7ade5c51fd9
aashiq4dev <ashiku161612@gmail.com>
2021-08-11 14:55:02 +0600

    int long value test with underscore

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         int myAge = 30;
         int herAge = myAge;
-        System.out.println(herAge);
+        int viewCount=123_456_789;
+        System.out.println(viewCount);
     }
 }
