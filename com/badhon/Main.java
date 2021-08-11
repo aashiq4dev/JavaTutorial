b1917378d5562f2c505e9639ee654744779eb8e9
aashiq4dev <ashiku161612@gmail.com>
2021-08-11 15:14:58 +0600

    refference var method

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -1,16 +1,13 @@
 package com.badhon;
 
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        int myAge = 30;
-        int herAge = myAge;
-        long viewCount = 3_123_456_789L;
-        char letter = 'A';
-        String letters = "HelloWorld"; // if you using charecter variable deffenetly use single coddetion or you Use
-                                       // String veriables so you need double codetion//
-        float price = 10.54F;
-        boolean isSelecetd = false;
-        System.out.println(isSelecetd);
+        byte age = 30;
+        Date now = new Date();
+        
+        System.out.println(now);
 
     }
 }
