dc26d3e91f7f2db79ed7deddfe2ea40c47ffce9e
aashiq4dev <ashiku161612@gmail.com>
2021-08-11 15:50:38 +0600

    Strings value

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -8,12 +8,7 @@ public class Main {
    
 
     public static void main(String[] args) throws Exception {
-       int x = 1;
-        int y = 1;
        
-        Point point1 = new Point(x,y);
-        Point point2 = point1; //they are "x","y" not indepentent
-        point1.x = 5;
-        System.out.println(point2);
+        System.out.println();
     }
 }
