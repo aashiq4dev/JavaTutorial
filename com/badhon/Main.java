a5c21b4a2f26c9da19d232d30916af9a343a5d7d
aashiq4dev <ashiku161612@gmail.com>
2021-08-11 15:34:18 +0600

    refference method

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -1,9 +1,19 @@
 package com.badhon;
 
-import java.util.Date;
+import java.awt.*;
+
+
 
 public class Main {
+   
+
     public static void main(String[] args) throws Exception {
-      
+       int x = 1;
+        int y = 1;
+       
+        Point point1 = new Point(x,y);
+        Point point2 = point1; //they are "x","y" not indepentent
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
