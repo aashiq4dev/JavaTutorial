b2710076af015597f38a9af9bf0605113c00254f
aashiq4dev <ashiku161612@gmail.com>
2021-08-11 18:12:07 +0600

    Array sort class complete

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -1,10 +1,16 @@
 package com.badhon;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
         //Array in java
+        int[] names = { 1, 2, 3 };
+        Arrays.sort(names);
+        System.out.println(Arrays.toString(names));
       
+    
     }
 
 
