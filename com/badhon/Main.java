77216278ef20568db27b83dbcdc2dc1d9dc6e021
aashiq4dev <ashiku161612@gmail.com>
2021-08-11 17:45:46 +0600

    new comitt dev-2 backslash

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         //Ecape Sequence
         String masseage = "c:\\Windows\\..";
+        //backSlash "//","/t" tab, "/n" method learning complete
         System.out.println(masseage);
     }
 
