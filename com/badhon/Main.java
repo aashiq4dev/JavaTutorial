2d1bdb2b4df81e2eb44c90183ade5c21f510a5d3
aashiq4dev <ashiku161612@gmail.com>
2021-08-11 17:53:35 +0600

    Array in java class start

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -3,10 +3,8 @@ package com.badhon;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        //Ecape Sequence
-        String masseage = "c:\\Windows\\..";
-        //backSlash "//","/t" tab, "/n" method learning complete
-        System.out.println(masseage);
+        //Array in java
+      
     }
 
 
