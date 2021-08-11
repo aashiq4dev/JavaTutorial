99bddf61a3f438c464b9399189a6825f223fbdc7
aashiq4dev <ashiku161612@gmail.com>
2021-08-11 15:05:12 +0600

    double var

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -4,9 +4,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         int myAge = 30;
         int herAge = myAge;
-        long viewCount=3_123_456_789L;
+        long viewCount = 3_123_456_789L;
         char letter = 'A';
-        String letters = "HelloWorld"; // if you using charecter variable deffenetly use single coddetion or you Use String veriables so you need double codetion//
-        System.out.println(letters);
+        String letters = "HelloWorld"; // if you using charecter variable deffenetly use single coddetion or you Use
+                                       // String veriables so you need double codetion//
+        double price = 10.54;
+
+        System.out.println(price);
+
     }
 }
