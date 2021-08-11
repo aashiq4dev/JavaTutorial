99d83c86f4a7950191d642bdd783a73b5d99a59b
aashiq4dev <ashiku161612@gmail.com>
2021-08-11 18:13:04 +0600

    Java multi dimantion arrays

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -5,10 +5,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        //Array in java
-        int[] names = { 1, 2, 3 };
-        Arrays.sort(names);
-        System.out.println(Arrays.toString(names));
+       
       
     
     }
