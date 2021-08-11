eaf32c566304d022a7bae0b3735d86ce368d0cc2
aashiq4dev <ashiku161612@gmail.com>
2021-08-11 14:55:55 +0600

    long veriable method with L

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         int myAge = 30;
         int herAge = myAge;
-        int viewCount=123_456_789;
+        long viewCount=3_123_456_789L;
         System.out.println(viewCount);
     }
 }
