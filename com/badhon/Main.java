4bcd7421f7f918629094d5c580019a43c68163ad
aashiq4dev <ashiku161612@gmail.com>
2021-08-12 12:42:14 +0600

    Formtting numbers start

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -5,9 +5,8 @@ package com.badhon;
 public class Main {
 
     public static void main(String[] object) throws Exception {
-        // math class and mathmatical operations
-        int result = (int)( Math.random()*100);
-       System.out.println(result);
+        // Formtting numbers
+        
 
     }
 
