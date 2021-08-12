32da09474e907d84d2af2f2351fbeb0223b1d13b
aashiq4dev <ashiku161612@gmail.com>
2021-08-12 17:50:13 +0600

    Reading input start

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -1,14 +1,8 @@
 package com.badhon;
-
-import java.text.NumberFormat;
-
 public class Main {
 
     public static void main(String[] object) throws Exception {
-       
-        String resutl=   NumberFormat.getPercentInstance().format(.1);
-       
-       System.out.println(resutl);
+       // Reading Input
 
     }
 
