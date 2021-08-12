efcc8553809438b4fe6f676e753a852511f8ed9e
aashiq4dev <ashiku161612@gmail.com>
2021-08-12 12:26:24 +0600

    math class and mathmatical operations

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -5,10 +5,8 @@ package com.badhon;
 public class Main {
 
     public static void main(String[] object) throws Exception {
-        // implicit casting
-        String x = "1";
-        int y = Integer.parseInt(x)  + 3;
-        System.out.println(y);
+        // math class and mathmatical operations
+       
 
     }
 
