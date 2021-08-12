c6edcacc0d488fb6dc1469dc4187e2e8e761fd15
aashiq4dev <ashiku161612@gmail.com>
2021-08-12 12:41:26 +0600

    mathmatical operation complete

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
     public static void main(String[] object) throws Exception {
         // math class and mathmatical operations
-       
+        int result = (int)( Math.random()*100);
+       System.out.println(result);
 
     }
 
