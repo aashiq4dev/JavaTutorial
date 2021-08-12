b9b21939cb62804f1a4d1491ee1989d8be6d735e
aashiq4dev <ashiku161612@gmail.com>
2021-08-12 10:48:09 +0600

    pactice java

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -1,16 +1,9 @@
 package com.badhon;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        // Arithmatic Expressions
-        
-
-      
-    
+      //pactice day
     }
-
 
 }
