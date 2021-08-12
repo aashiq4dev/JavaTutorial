6780a1b626e6efad6beb6c6f22b3cef346569086
aashiq4dev <ashiku161612@gmail.com>
2021-08-12 18:16:05 +0600

    Reading Input part complete

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -1,9 +1,15 @@
 package com.badhon;
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] object) throws Exception {
-       // Reading Input
-
-    }
+    public static void main(String[] args) throws Exception {
+        // Reading Input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scanner.nextLine().trim();
+        System.out.println(" You are " + name);
+    }  
 
 }
