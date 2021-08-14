0fdb31b280882867429f3c1265528f5d2755769b
aashiq4dev <ashiku161612@gmail.com>
2021-08-14 12:33:57 +0600

    forloop and  While as a same think for apply

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -1,29 +1,20 @@
 package com.badhon;
 
-import java.text.NumberFormat;
 import java.util.Scanner;
 
-import javax.lang.model.util.ElementScanner14;
-
-import jdk.jfr.Percentage;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         // FizzBuzz
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Number:");
-        int number = scanner.nextInt();
-        if (number % 5 == 0 ) {
-            System.out.println("Fizz");
-        } else if (number % 3 == 0) {
-            System.out.println("Buzz");
-        } else if(number%5==0&& number%3==0){
-            System.out.println("FizzBuzz");
+        for (int i = 5; i > 0; i--) {
+            System.out.println("Hello World" + i);
+
         }
-         else 
-            System.err.println(number);
-
+        int i = 0;
+            while (i>0) {
+                System.out.println("Hello World" + i);
+                i--;
+            }
     }
-
 }
