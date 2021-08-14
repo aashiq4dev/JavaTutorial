218e2b51eda1457777fd1741973773df553a67d7
aashiq4dev <ashiku161612@gmail.com>
2021-08-14 09:01:49 +0600

    step 2 wrong systems to apply

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -14,11 +14,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Number:");
         int number = scanner.nextInt();
-        if (number % 5 == 0 && number % 3 == 0) {
-            System.out.println("FizzBuzz");
+        if (number % 5 == 0 ) {
+            System.out.println("Fizz");
         } else if (number % 3 == 0) {
             System.out.println("Buzz");
-        } else
+        } else if(number%5==0&& number%3==0){
+            System.out.println("FizzBuzz");
+        }
+         else 
             System.err.println(number);
 
     }
