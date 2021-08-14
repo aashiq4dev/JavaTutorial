05be2da3effd27f18b837b03a139a3909b3c7739
aashiq4dev <ashiku161612@gmail.com>
2021-08-14 09:00:02 +0600

    step 1 rigth systems

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -5,15 +5,22 @@ import java.util.Scanner;
 
 import javax.lang.model.util.ElementScanner14;
 
-
 import jdk.jfr.Percentage;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         // FizzBuzz
-        
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number:");
+        int number = scanner.nextInt();
+        if (number % 5 == 0 && number % 3 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (number % 3 == 0) {
+            System.out.println("Buzz");
+        } else
+            System.err.println(number);
+
     }
 
 }
