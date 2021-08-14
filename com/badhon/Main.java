2b90306a50e7f349ba882f1804e9e2a7bca985d6
aashiq4dev <ashiku161612@gmail.com>
2021-08-14 13:00:34 +0600

    do while loops systems

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -2,16 +2,22 @@ package com.badhon;
 
 import java.util.Scanner;
 
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        String input = "";
+        // While loops
         Scanner scanner = new Scanner(System.in);
+        String input = "";
         while (!input.equals("quit")) {
-            System.out.println("Input:");
-            input = scanner.next();
-           System.out.println(input);
+            System.out.print("Input:");
+            input = scanner.next().toLowerCase().trim();
+            System.out.println(input);
         }
+        //do while loops
+        do{
+            System.out.print("Input:");
+            input = scanner.next().toLowerCase().trim();
+            System.out.println(input);
+        } while (input.equals("quit"));
     }
 }
