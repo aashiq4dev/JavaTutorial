ff5d806d42c2bee7a365485a0f1aa3fa05097c50
aashiq4dev <ashiku161612@gmail.com>
2021-08-14 08:54:40 +0600

    FizzBuzz systems

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -11,17 +11,8 @@ import jdk.jfr.Percentage;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        // conditional statements
+        // FizzBuzz
         
-          
-        int income = 10_000;
-        
-        Scanner scanner = new Scanner(System.in);
-       
-        System.out.print("Your income: ");
-        int inputIncome = scanner.nextInt();
-        String className = inputIncome < income ? "Your class is Economy" : "You're a VIP";
-        System.out.println(className);
         
     }
 
