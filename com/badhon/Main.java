44c689ca42d042a83713d958b5080ebb1bd7927d
aashiq4dev <ashiku161612@gmail.com>
2021-08-14 08:16:00 +0600

    conditional statements

--- com/badhon/Main.java
+++ com/badhon/Main.java
@@ -3,15 +3,26 @@ package com.badhon;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
+import javax.lang.model.util.ElementScanner14;
+
+
 import jdk.jfr.Percentage;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        // Control Flow
-        int x = 1;
-        int y = 2;
-        System.out.println(x >= y);
+        // conditional statements
+        
+          
+        int income = 10_000;
+        
+        Scanner scanner = new Scanner(System.in);
+       
+        System.out.print("Your income: ");
+        int inputIncome = scanner.nextInt();
+        String className = inputIncome < income ? "Your class is Economy" : "You're a VIP";
+        System.out.println(className);
+        
     }
 
 }
